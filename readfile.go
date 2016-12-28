72779030ff5af0de5be96419c7ff68a3b87171bb
刘晓军 <gb2312@163.com>
2016-12-28 11:01:10 +0800

    read file

---
+++ readfile.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	userFile := "../data/internet_20161227_00.txt"
+	fin, err := os.Open(userFile)
+	defer fin.Close()
+	if err != nil {
+		fmt.Println(userFile, err)
+		return
+	}
+	buf := make([]byte, 1024)
+	for {
+		n, _ := fin.Read(buf)
+		if 0 == n {
+			break
+		}
+		os.Stdout.Write(buf[:n])
+	}
+}
